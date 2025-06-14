e0988298e1a101c865c4bc7468aa818df285f0ca
Osni Filipo <osnifilipo@gmail.com>
2025-06-14 20:27:35 -0300

    up

---
+++ main.py
@@ -0,0 +1,5 @@
+# Importando a biblioteca pywhatkit para enviar mensagens no WhatsApp
+import pywhatkit as kit 
+
+# Envia uma mensagem instantânea no WhatsApp simples
+kit.sendwhatmsg_instantly("+5511984288654", "Teste de mensagem !")
